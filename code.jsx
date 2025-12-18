d32c741a8a9219dd293267b41f9932734f38218f
Noctis-Lucis96 <ishithasudeepa@gmail.com>
2025-12-18 21:44:00 +0530

    mn Noctis

--- code.jsx
+++ code.jsx
@@ -1,5 +1,6 @@
 const myElement = <h1>I Love JSX! Code 1</h1>;
 const myElement2 = <h1>I Love JSX! Code 2</h1>;
+// mn Noctis
 createRoot(document.getElementById('root')).render(
   myElement,
   myElement2
